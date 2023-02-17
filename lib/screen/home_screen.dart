ad4dbe11152e782af203dcc87331e3d2e9942eaa
Mseunghwan <minccc42710@gmail.com>
2023-02-18 02:03:23 +0900

    map create

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,13 +12,62 @@ class _HomeScreenState extends State<HomeScreen> {
   // latitude - 위도 , logitude - 경도
   static final LatLng companyLatLng = LatLng(37.5233273, 126.921252);
 
+  // zoom 정보도 줘야
+  static final CameraPosition initialPosition = CameraPosition(
+    target: companyLatLng,
+    zoom: 15,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+      appBar: renderAppBar(),
+      body: Column(
+        children: [
+          _CustomGoogleMap(initialPosition: initialPosition),
+          _ChoolCheckButton(),
+        ],
       ),
+    );
+  }
+}
+
+AppBar renderAppBar() {
+  return AppBar(
+    title: Text(
+      '오늘도 출근',
+      style: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+    backgroundColor: Colors.white,
+  );
+}
+
+class _CustomGoogleMap extends StatelessWidget {
+  final CameraPosition initialPosition;
+  const _CustomGoogleMap({required this.initialPosition, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: GoogleMap(
+        mapType: MapType.satellite,
+        initialCameraPosition: initialPosition,
+      ),
+    );
+  }
+}
+
+class _ChoolCheckButton extends StatelessWidget {
+  const _ChoolCheckButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text('출근'),
     );
   }
 }
